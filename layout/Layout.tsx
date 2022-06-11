91555598af81ff7243c6dbf85516cd21f5d35c61
Alson33 <garbuja12alson@gmail.com>
2022-06-11 23:03:10 +0545

    [responsive]: make the landing page responsive, though still need work

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
+import { GitHub, Menu, Moon, Search, Twitter, X } from "react-feather";
 
 const Layout = ({ children }: { children: JSX.Element }) => {
+  const [isNavOpen, setIsNavOpen] = useState(false)
+
   const toggleTheme = () => {
     const theme = localStorage.getItem("notetal_theme__mode");
     if (theme && theme === "light") {
@@ -11,21 +14,35 @@ const Layout = ({ children }: { children: JSX.Element }) => {
     document.body.classList.toggle("dark");
   };
 
+  const openNavMenu = () => setIsNavOpen(prev => !prev)
+
   return (
     <div className="bg-white text-black dark:bg-gray-800 dark:text-white">
       <div className=" w-4/5 mx-auto">
-        <header className="flex justify-between py-10">
+        <header className="flex justify-between items-center py-10">
           <span>LOGO</span>
-          <nav>
-            <ul className="flex justify-between">
-              <li className="px-2 mr-4 cursor-pointer">Git</li>
-              <li className="px-2 mr-4 cursor-pointer">Twitter</li>
-              <li className="px-2 mr-4 cursor-pointer">Search</li>
-              <li className="px-2 mr-4 cursor-pointer" onClick={toggleTheme}>
-                Dark
+          <Menu className="block md:hidden cursor-pointer" onClick={openNavMenu} />
+          <nav className={
+            `fixed w-full h-screen ${isNavOpen?'top-0':'-top-full'} left-0 z-10 py-10 
+              md:relative md:w-fit md:h-fit md:p-0 
+              bg-white dark:bg-gray-800
+              transition-all duration-500
+            `
+          }>
+            <X className="fixed z-20 right-12 cursor-pointer md:hidden" onClick={openNavMenu} />
+            <ul className="h-full flex justify-between items-center flex-col md:flex-row gap-4">
+              <li className="px-2 mr-4 cursor-pointer flex"><GitHub /><span className="ml-2 block md:hidden">Github</span></li>
+              <li className="px-2 mr-4 cursor-pointer flex"><Twitter /><span className="ml-2 block md:hidden">Twitter</span></li>
+              <li className="px-2 mr-4 cursor-pointer flex"><Search /><span className="ml-2 block md:hidden">Search</span></li>
+              <li className="px-2 mr-4 cursor-pointer flex" onClick={toggleTheme}>
+                <Moon /><span className="ml-2 block md:hidden">Dark mode</span>
               </li>
-              <li className="px-2 mr-4 cursor-pointer">Login</li>
-              <li className="px-2 mr-4 cursor-pointer">Signup</li>
+              <li className="mr-4 cursor-pointer border border-indigo-400 py-2 px-4">
+                Login
+              </li>
+              <li className="mr-4 cursor-pointer bg-indigo-400 text-white py-2 px-4">
+                Signup
+              </li>
             </ul>
           </nav>
         </header>
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,41 +14,41 @@ const Home: NextPage = () => {
       </Head>
 
       <article className="py-5">
-        <section className='text-center h-60 my-11'>
+        <section className='text-center my-11'>
             <h1>Welcome to <span className="text-indigo-400">Notetal</span></h1>
             <p>
               Complete portal for all the notes you are ever looking for.
             </p>
-            <button className="py-2 px-5 w-64 bg-indigo-400 rounded-full mt-10">
+            <button className="py-2 px-5 w-64 bg-indigo-400 text-white rounded-full mt-10">
               Get Started
             </button>
         </section>
         <section className="my-10">
-          <h3 className="text-center">Features</h3>
-          <div className="flex justify-between items-center gap-4">
+          {/* <h3 className="text-center">Features</h3> */}
+          <div className="flex flex-col md:flex-row justify-between items-center gap-4 my-5">
             <Image src="/editor.png" alt="powerful editor" width="500" height="500" />
-            <div className="w-1/2">
-              <h4>Powerful Editor</h4>
+            <div className="w-full md:w-1/2">
+              <h4 className="text-indigo-400">Powerful Editor</h4>
               <p>
                 With the inbuilt powerful editor not only for normal notes but also
                 for mathematical equations take notes in seconds.
               </p>
             </div>
           </div>
-          <div className="flex justify-between items-center flex-row-reverse gap-4">
+          <div className="flex flex-col md:flex-row-reverse justify-between items-center gap-4 my-5">
             <Image src="/something.png" alt="powerful editor" width="500" height="500" />
-            <div className="w-1/2">
-              <h4>Some features</h4>
+            <div className="w-full md:w-1/2">
+              <h4 className="text-indigo-400">Some features</h4>
               <p>
                 With the inbuilt powerful editor not only for normal notes but also
                 for mathematical equations take notes in seconds.
               </p>
             </div>
           </div>
-          <div className="flex justify-between items-center gap-4">
+          <div className="flex flex-col md:flex-row justify-between items-center gap-4 my-5">
             <Image src="/mobileUI.png" alt="powerful editor" width="500" height="500" />
-            <div className="w-1/2">
-              <h4>Mobile friendly</h4>
+            <div className="w-full md:w-1/2">
+              <h4 className="text-indigo-400">Mobile friendly</h4>
               <p>
                 Want to read outside, on bed, while waiting for someone. With the mobile
                 friendly UI of notetal you can read and write anywhere and anytime.
