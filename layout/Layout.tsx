37a96927d57b38717011d4a6d7de0182459dd0f6
Alson33 <garbuja12alson@gmail.com>
2022-06-11 16:44:32 +0545

    [feature]: add a dark mode toggling function

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,29 +1,39 @@
-import React from 'react'
+import React from "react";
 
-const Layout = ({ children } : {
-    children: JSX.Element,
-}) => {
+const Layout = ({ children }: { children: JSX.Element }) => {
+  const toggleTheme = () => {
+    const theme = localStorage.getItem("notetal_theme__mode");
+    if (theme && theme === "light") {
+      localStorage.setItem("notetal_theme__mode", "dark");
+    } else {
+      localStorage.setItem("notetal_theme__mode", "light");
+    }
+    document.body.classList.toggle("dark");
+  };
+
   return (
-    <>
-        <header className="flex justify-between w-4/5 mx-auto py-10">
-            <span>LOGO</span>
-            <nav>
-                <ul className="flex justify-between">
-                    <li className='px-2 mr-4'>Git</li>
-                    <li className='px-2 mr-4'>Twitter</li>
-                    <li className='px-2 mr-4'>Search</li>
-                    <li className='px-2 mr-4'>Dark</li>
-                    <li className='px-2 mr-4'>Login</li>
-                    <li className='px-2 mr-4'>Signup</li>
-                </ul>
-            </nav>
+    <div className="bg-white text-black dark:bg-gray-800 dark:text-white">
+      <div className=" w-4/5 mx-auto">
+        <header className="flex justify-between py-10">
+          <span>LOGO</span>
+          <nav>
+            <ul className="flex justify-between">
+              <li className="px-2 mr-4 cursor-pointer">Git</li>
+              <li className="px-2 mr-4 cursor-pointer">Twitter</li>
+              <li className="px-2 mr-4 cursor-pointer">Search</li>
+              <li className="px-2 mr-4 cursor-pointer" onClick={toggleTheme}>
+                Dark
+              </li>
+              <li className="px-2 mr-4 cursor-pointer">Login</li>
+              <li className="px-2 mr-4 cursor-pointer">Signup</li>
+            </ul>
+          </nav>
         </header>
-        <main>
-            {children}
-        </main>
+        <main>{children}</main>
         <footer></footer>
-    </>
-  )
-}
+      </div>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,21 @@ import 'tailwindcss/tailwind.css';
 
 import type { AppProps } from "next/app";
 import Layout from "../layout/Layout";
+import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    const themeMode = localStorage.getItem('notetal_theme__mode')
+    if(themeMode){
+      if(themeMode==='dark'){
+        document.body.classList.add('dark')
+      }else{
+        document.body.classList.remove('dark')
+      }
+    }
+  }, [])
+
   return (
     <Layout>
       <Component {...pageProps} />
