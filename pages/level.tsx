9a42162d5bb3bd71722c1ef9591b7e92f130be94
Alson33 <garbuja12alson@gmail.com>
2022-06-19 14:25:51 +0545

    [link]: add link to notes page

--- pages/level.tsx
+++ pages/level.tsx
@@ -11,13 +11,13 @@ const Level = () => {
       </Head>
       <h3 className="text-center">Choose note to read</h3>
       <div className="flex flex-wrap gap-4 my-6 justify-center">
-        <LevelBox title="Software Engineering" href="/" description="Read notes from software engineering syllabus based on PU" />
-        <LevelBox title="Civil Engineering" href="/" />
-        <LevelBox title="MBBS" href="/" />
-        <LevelBox title="BBA" href="/" />
-        <LevelBox title="GIT" href="/" />
-        <LevelBox title="LAW" href="/" />
-        <LevelBox title="Lok sewa" href="/" />
+        <LevelBox title="Software Engineering" href="/notes/test/12345" description="Read notes from software engineering syllabus based on PU" />
+        <LevelBox title="Civil Engineering" href="/notes/test/12345" />
+        <LevelBox title="MBBS" href="/notes/test/12345" />
+        <LevelBox title="BBA" href="/notes/test/12345" />
+        <LevelBox title="GIT" href="/notes/test/12345" />
+        <LevelBox title="LAW" href="/notes/test/12345" />
+        <LevelBox title="Lok sewa" href="/notes/test/12345" />
       </div>
     </div>
   );
