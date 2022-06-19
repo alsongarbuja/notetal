b262cd27974041cb7f9f3c545c3da86978369c1b
Alson33 <garbuja12alson@gmail.com>
2022-06-19 14:25:34 +0545

    [remove]: remove home module css file

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,12 +2,10 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
-
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Notetal</title>
         <meta name="description" content="Portal for note taking as well as reading" />
