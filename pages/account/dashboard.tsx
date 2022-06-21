52569f2308478efd0acb428f19f0b986a44c6e15
Alson33 <garbuja12alson@gmail.com>
2022-06-21 22:46:56 +0545

    [responsive]: dashboard responsive

--- pages/account/dashboard.tsx
+++ pages/account/dashboard.tsx
@@ -9,8 +9,8 @@ const Dashboard = () => {
             <title>Notetal - Dashboard</title>
         </Head>
         <h2>Dashboard</h2>
-        <div className="flex gap-5 mt-2">
-            <div className="w-52 border px-5 py-4 rounded-md text-center border-indigo-400 bg-indigo-400/10 dark:text-indigo-200">
+        <div className="flex gap-5 mt-2 flex-wrap">
+            <div className="w-full sm:w-52 border px-5 py-4 rounded-md text-center border-indigo-400 bg-indigo-400/10 dark:text-indigo-200">
                 <h2>4</h2>
                 <h4 className="mb-3 flex gap-2 justify-center">
                     <Folder /> Levels
@@ -21,7 +21,7 @@ const Dashboard = () => {
                     </span>
                 </Link>
             </div>
-            <div className="w-52 border px-5 py-4 rounded-md text-center border-indigo-400 bg-indigo-400/10 dark:text-indigo-200">
+            <div className="w-full sm:w-52 border px-5 py-4 rounded-md text-center border-indigo-400 bg-indigo-400/10 dark:text-indigo-200">
                 <h2>9</h2>
                 <h4 className="mb-3 flex gap-2 justify-center">
                     <Book className="inline" /> Subject
@@ -32,7 +32,7 @@ const Dashboard = () => {
                     </span>
                 </Link>
             </div>
-            <div className="w-52 border px-5 py-4 rounded-md text-center border-indigo-400 bg-indigo-400/10 dark:text-indigo-200">
+            <div className="w-full sm:w-52 border px-5 py-4 rounded-md text-center border-indigo-400 bg-indigo-400/10 dark:text-indigo-200">
                 <h2>42 </h2>
                 <h4 className="mb-3 flex gap-2 justify-center">
                     <List /> Topics
