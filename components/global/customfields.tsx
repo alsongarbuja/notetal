644efece66d14671cd256bbcb6ca2da270382ef7
Alson33 <garbuja12alson@gmail.com>
2022-07-08 19:27:49 +0545

    [component]: add a custom select field

--- components/global/customfields.tsx
+++ components/global/customfields.tsx
@@ -48,3 +48,51 @@ export const CustomInput = ({
     </div>
   );
 };
+
+export const CustomSelect = ({
+  label,
+  isRequired = true,
+  name,
+  placeholder,
+  id,
+  value="",
+  onChange,
+  classes="col-span-1",
+  options
+}: {
+  label: string;
+  isRequired?: boolean;
+  id?: string;
+  name: string,
+  placeholder?: string,
+  value?: string | number,
+  onChange: ChangeEventHandler,
+  classes?: string, 
+  options: { name: string, value: string }[],
+}) => {
+  return (
+    <div className={`mt-2 ${classes}`}>
+      <label htmlFor={id} className="block text-sm font-medium ">
+        {label}
+      </label>
+      <div className="mt-1">
+        <select
+          id={id}
+          name={name}
+          value={value}
+          required={isRequired}
+          placeholder={placeholder}
+          onChange={onChange}
+          className="text-black appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+        >
+          <option value="">-- Select --</option>
+          {
+            options.map(option => (
+              <option value={option.value} key={option.value}>{option.name}</option>
+            ))
+          }
+        </select>
+      </div>
+    </div>
+  )
+}
