4828ec26f05bfa64c6092c0c8ffaf660e8900b57
Alson33 <garbuja12alson@gmail.com>
2022-09-18 10:10:22 +0545

    [component]: custom switch component add

--- components/global/customfields.tsx
+++ components/global/customfields.tsx
@@ -96,3 +96,33 @@ export const CustomSelect = ({
     </div>
   )
 }
+
+export const CustomSwitch = ({
+  classes,
+  id,
+  value,
+  label,
+  name,
+  onChange,
+}: {
+  classes?: string;
+  id: string;
+  value: boolean;
+  label: string;
+  name: string;
+  onChange: ChangeEventHandler;
+}) => {
+  return (
+    <div className={`mt-2 ${classes}`}>
+      <p className="block text-sm font-medium">
+        {label}
+      </p>
+      <div className="mt-1">
+        <label htmlFor={id} className="inline-flex relative items-center cursor-pointer">
+        <input name={name} type="checkbox" id={id} className="sr-only peer" checked={value} onChange={onChange} />
+          <div className="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+        </label>
+      </div>
+    </div>
+  )
+}
