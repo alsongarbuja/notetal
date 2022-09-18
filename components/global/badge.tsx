7838b68f9d71373a04616dbbcd9241982720ab6e
Alson33 <garbuja12alson@gmail.com>
2022-09-18 16:47:54 +0545

    [ui]: color fixes for badge

--- components/global/badge.tsx
+++ components/global/badge.tsx
@@ -14,28 +14,24 @@ const Badge = ({
 
   const colorScheme = color === 'green' ? {
     bg: 'bg-green-200/30',
-    text: 'text-white',
     border: 'border-green-600/50',
     solid: 'bg-green-500',
     solidText: 'text-white',
   } 
     : color === 'red' ? {
       bg: 'bg-red-100',
-      text: 'text-red-800',
       border: 'border-red-300',
       solid: 'bg-red-500',
       solidText: 'text-white',
     } 
     : color === 'blue' ? {
       bg: 'bg-blue-100',
-      text: 'text-blue-800',
       border: 'border-blue-300',
       solid: 'bg-blue-500',
       solidText: 'text-white',
     } 
     : {
       bg: 'bg-purple-100',
-      text: 'text-purple-800',
       border: 'border-purple-300',
       solid: 'bg-purple-500',
       solidText: 'text-white',
@@ -44,9 +40,9 @@ const Badge = ({
   return (
     <div
       className={`
-        px-2 font-thin text-xs
+        px-2 font-thin text-xs 
         ${shape === 'rounded' ? 'rounded-full' : 'rounded-sm'}
-        ${variant === 'solid' ? `${colorScheme.solid} ${colorScheme.solidText}` : `border-2 ${colorScheme.text} ${colorScheme.bg} ${colorScheme.border}`}
+        ${variant === 'solid' ? `${colorScheme.solid} ${colorScheme.solidText} text-white` : `border-2 text-black dark:text-white ${colorScheme.bg} ${colorScheme.border}`}
       `}
     >
       {label}
