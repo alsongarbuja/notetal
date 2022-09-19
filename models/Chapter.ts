5ef61206ba9f735d7d738ce915e90a5dea52ff13
Alson33 <garbuja12alson@gmail.com>
2022-09-19 12:28:24 +0545

    [model]: add chapters model schema

---
+++ models/Chapter.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+
+const ChapterSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    slug: {
+        type: String,
+        unique: true,
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+    hasLesson: {
+        type: Boolean,
+        default: false,
+    },
+    createdBy: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'User',
+        required: true,
+    }
+}, { timestamps: true })
+
+ChapterSchema.pre('save', async function (next) {
+    const chapters = this;
+    chapters.slug = chapters.name.toLowerCase().split(" ").join("-")
+    next();
+});
+
+export default mongoose.models.Chapters || mongoose.model('Chapters', ChapterSchema)
