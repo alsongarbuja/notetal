8922b5211d8e865c372615ce610c3abced6c5f4c
Alson33 <garbuja12alson@gmail.com>
2022-10-01 15:09:29 +0545

    [type]: custom types of app

---
+++ types/custom.ts
@@ -0,0 +1,4 @@
+export type dynamicStringObject = {[key: string]: string}
+export type dynamicObject = {[key: string]: any}
+
+export type modalTypes = "user" | "note" | "subnote" | "page" | "chapter" | "lesson";
