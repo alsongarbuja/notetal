36be51f447e2cb844220341a142eaaec7db42f22
Alson33 <garbuja12alson@gmail.com>
2022-10-01 14:39:48 +0545

    [feature]: error handling

--- components/global/customfields.tsx
+++ components/global/customfields.tsx
@@ -50,7 +50,8 @@ export const CustomInput = ({
           className={`
             text-black 
             appearance-none block w-full px-3 py-2 
-            ${hasError && "border-red-500 border-2"} 
+            border
+            ${hasError ? "border-red-500 border-2" : "border-gray-300"} 
             rounded-md shadow-sm 
             placeholder-gray-400 
             focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 
---
+++ helpers/backend/utils/apiError.ts
@@ -0,0 +1,9 @@
+export const apiError = (error: any) => {
+    const { message } = error;
+
+    const errors = message.split(":")
+    const errorObject = {
+        [errors[1].slice(1)]: errors[2],
+    }
+    return errorObject
+}
